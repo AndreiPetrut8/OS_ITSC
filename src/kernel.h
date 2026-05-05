23b8bf2cd79e883f0762df6fcf5d0167d9252ee4
DavidOprea18 <david-a.oprea@student.upt.ro>
2026-05-06 01:14:25 +0300

    added wait_process

--- src/kernel.h
+++ src/kernel.h
@@ -29,14 +29,14 @@ typedef struct
     int wait_ticks;
 } pcb_t;
 
-
-void kprint(const char* str);
+void kprint(const char *str);
 void kprint_int(int num);
 void kprint_hex(uint32_t n);
 void kprint_newline(void);
 void kclear_screen(void);
 void list_processes();
 void kill_process(int pid);
+void wait_process(int pid, int ticks);
 void yield();
 void scheduler_tick();
 
